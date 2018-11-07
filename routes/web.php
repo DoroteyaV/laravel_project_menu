6c9101c20d312a39038a42ec2314cd032acf8f1c
Angel <achi_iv@abv.bg>
2018-11-07 14:55:44 +0200

    add web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/restaurant', 'RestaurantsController@index')->name('restaurant');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
